82328f32a6fc8ac757347ce83d574a422ebde63f
chenHusky <377075038@qq.com>
2023-03-07 20:24:19 +0800

    tuicu

--- packages/mindspore/src/views/user-login/TheLogout.vue
+++ packages/mindspore/src/views/user-login/TheLogout.vue
@@ -5,10 +5,11 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 onMounted(() => {
   const redirect_uri = route.query.redirect_uri as string;
+  const id_token = route.query.id_token as string;
   if (redirect_uri) {
     const { token } = getUserAuth();
     if (token) {
-      logout({ community: 'mindspore' }, redirect_uri);
+      logout({ community: 'mindspore', id_token }, redirect_uri);
     } else {
       location.href = redirect_uri;
     }
