1c49b2fef1100711b4e8622e168b398576fd0338
chenHusky <377075038@qq.com>
2023-07-17 15:17:09 +0800

    rsa加密插件更改

--- packages/shared/utils/rsa.ts
+++ packages/shared/utils/rsa.ts
@@ -1,11 +1,26 @@
 import { getPublicKey } from '../api/api-login';
 import JsEncrypt from 'jsencrypt';
 import { b64tohex } from './base64';
+import forge from 'node-forge';
 /**
  * @word 要加密的内容
  * @keyWord String  服务器随机返回的关键字
  *  */
 export function rsaEncrypt(word: string, keyWord: string): string {
+  if (import.meta.env?.VITE_COMMUNITY === 'opengauss') {
+    const key = `-----BEGIN PUBLIC KEY-----\n${keyWord}\n-----END PUBLIC KEY-----`;
+    const publicKey = forge.pki.publicKeyFromPem(key);
+    const buffer = forge.util.createBuffer(word, 'utf8');
+    const bytes = buffer.getBytes();
+    return forge.util.bytesToHex(
+      publicKey.encrypt(bytes, 'RSA-OAEP', {
+        md: forge.md.sha256.create(),
+        mgf1: {
+          md: forge.md.sha1.create(),
+        },
+      })
+    );
+  }
   const encrypt = new JsEncrypt();
   encrypt.setPublicKey(keyWord);
   return b64tohex(encrypt.encrypt(word) as string) || '';
