fb271838a776411745728ebe1247fe66acdc4489
chenHusky <377075038@qq.com>
2023-08-28 20:20:52 +0800

    login

--- packages/euler/src/views/user-login/components/LoginForm.vue
+++ packages/euler/src/views/user-login/components/LoginForm.vue
@@ -220,7 +220,10 @@ const showRestrictedTip = computed(
   () => !hiddenRestrictedTipArr.includes(loginParams.value.client_id)
 );
 const accountPlaceholder = computed(() => {
-  if (type.value === 'register' && selectLoginType.value === 'password') {
+  if (
+    (type.value === 'register' && selectLoginType.value === 'password') ||
+    !showSwitch.value
+  ) {
     return i18n.value.ENTER_YOUR_EMAIL;
   } else if (type.value === 'login' && selectLoginType.value === 'password') {
     return i18n.value.ENTER_YOUR_ACCOUNT;
