b8b2ee3a1f0bd6927b428974ebd611376c2c956e
chenHusky <377075038@qq.com>
2024-04-11 16:09:11 +0800

    mindspore不需要注册补充

--- packages/mindspore/src/shared/const.ts
+++ packages/mindspore/src/shared/const.ts
@@ -1,3 +1,10 @@
 export const ONLY_LOGIN_ID = [
+  '641e9462ccdac11ce37f6f56',
+  '62a2f9596b35a2a0330c3e6a',
+  '62849faf9ee1a5d5d6aadde2',
+  '62845bb7b52bbf9a67a76e26',
   '64ae9734f3247259a5f2b095',
+  '6284c350348462b176a03bca',
+  '638edd99f9080f705a561218',
+  '6284bba4cda4351c53b8c7ff',
 ];
