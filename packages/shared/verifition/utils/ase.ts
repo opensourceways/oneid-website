4023ad7c49ea28dc8e49ab7cf978e6028f0b4e89
guozhen <1084235395@qq.com>
2024-10-28 18:10:02 +0800

    fix: 送检，去掉默认参数

--- packages/shared/verifition/utils/ase.ts
+++ packages/shared/verifition/utils/ase.ts
@@ -3,7 +3,7 @@ import CryptoJS from 'crypto-js';
  * @word 要加密的内容
  * @keyWord String  服务器随机返回的关键字
  *  */
-export function aesEncrypt(word: string, keyWord = 'XwKsGlMcdPMEhR1B') {
+export function aesEncrypt(word: string, keyWord: string) {
   const key = CryptoJS.enc.Utf8.parse(keyWord);
   const srcs = CryptoJS.enc.Utf8.parse(word);
   const encrypted = CryptoJS.AES.encrypt(srcs, key, {
