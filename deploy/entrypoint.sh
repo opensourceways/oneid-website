cbbcdca52520e0225b255976f880643423989261
guozhen <1084235395@qq.com>
2024-10-30 17:42:20 +0800

    fix: 恢复nginx、docker配置

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export FLOATING_IP=$(ifconfig | grep 'inet' | awk '{print $2}' | head -n 1)
+# export FLOATING_IP=$(ifconfig | grep 'inet' | awk '{print $2}' | head -n 1)
 
 bash /etc/nginx/monitor.sh $DET_URL $DST_PATH &
 /usr/share/nginx/sbin/nginx -g 'daemon off;'
