edada7fdb4bd603f11aa1b1e6dcf5b5291964c27
guozhen <1084235395@qq.com>
2024-10-30 15:09:13 +0800

    fix: 恢复nginx配置

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#FLOATING_IP=$(hostname -I | awk '{print $1}')
-
-#cat <<EOL > /etc/nginx/nginx.conf
-
 bash /etc/nginx/monitor.sh $DET_URL $DST_PATH &
 /usr/share/nginx/sbin/nginx -g 'daemon off;'
