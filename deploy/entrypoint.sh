780fc636f4b4ca00f144969b8b5399aaeb8c4ef6
guozhen <1084235395@qq.com>
2024-11-01 17:44:48 +0800

    fix: 送检，全零ip问题修改

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# export FLOATING_IP=$(ifconfig | grep 'inet' | awk '{print $2}' | head -n 1)
+export FLOATING_IP=$(ifconfig | grep 'inet' | awk '{print $2}' | head -n 1)
+sed -i "s|\$FLOATING_IP|${FLOATING_IP}|g" /etc/nginx/nginx.conf
 
 bash /etc/nginx/monitor.sh $DET_URL $DST_PATH &
 /usr/share/nginx/sbin/nginx -g 'daemon off;'
