5d02c6c0f96e74c48e0fb558c236dd0975a59a40
guozhen <1084235395@qq.com>
2024-11-04 20:12:09 +0800

    fix: 送检，nginx和docker配置恢复

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-export FLOATING_IP=$(ifconfig | grep 'inet' | awk '{print $2}' | head -n 1)
-sed -i "s|\$FLOATING_IP|${FLOATING_IP}|g" /etc/nginx/nginx.conf
-
 bash /etc/nginx/monitor.sh $DET_URL $DST_PATH &
 /usr/share/nginx/sbin/nginx -g 'daemon off;'
